26b3738550ba93241342296cc1c605813e3d91c5
Christina Harris <christinathedesigner@outlook.com>
2020-05-19 08:57:50 -0700

    testing complete and super fun

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,25 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  const e = item.enhancement === 20 ? 20 : item.enhancement + 1
+  // if (item.enhancement == 20) {
+  //   return {...item}
+  // }
+   // return { ...item, enhancement: item.enhancement + 1 };
+  return { ...item, enhancement: e };
 }
 
 function fail(item) {
-  return { ...item };
+  const d = item.enhancement < 15 ? item.durability - 5 : item.durability - 10
+  const e =  item.enhancement > 16 ? item.enhancement - 1 : item.enhancement
+  return { ...item, durability:d, enhancement:e };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability:100 };
 }
 
 function get(item) {
-  return { ...item };
+  const n = item.enhancement > 0 ? `[+${item.enhancement}] ${item.name}` : item.name
+  return { ...item, name:n };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,68 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("test repair", () => {
+  //act
+  it("should increase durability to 100", () => {
+    const item = { name: "sword", enhancement: 15, durability: 80 };
+    const result = enhancer.repair(item);
+    expect(result.durability).toBe(100);
+  });
+});
+
+describe("test succeeds", () => {
+  //act
+  it("should increase enhancement by 1", () => {
+    const item = { name: "sword", enhancement: 15, durability: 80 };
+    const result = enhancer.succeed(item);
+    expect(result.enhancement).toBe(16);
+  });
+
+  it("should not increase enhancement if 20", () => {
+    const item = { name: "sword", enhancement: 20, durability: 80 };
+    const result = enhancer.succeed(item);
+    expect(result.enhancement).toBe(20);
+  });
+
+  it("should not change durability", () => {
+    const item = { name: "sword", enhancement: 20, durability: 80 };
+    const result = enhancer.succeed(item);
+    expect(result.durability).toBe(80);
+  });
+});
+
+
+describe("test fails", () => {
+  //act
+  it("enhancement less than 15", () => {
+    const item = { name: "sword", enhancement: 10, durability: 80 };
+    const result = enhancer.fail(item);
+    expect(result.durability).toBe(75);
+  });
+
+  it("enhancement greater than 15", () => {
+    const item = { name: "sword", enhancement: 20, durability: 80 };
+    const result = enhancer.fail(item);
+    expect(result.durability).toBe(70);
+  });
+
+  it("enhancement level is decreased", () => {
+    const item = { name: "sword", enhancement: 18, durability: 80 };
+    const result = enhancer.fail(item);
+    expect(result.enhancement).toBe(17);
+  });
+});
+
+describe("test get", () => {
+  //act
+  it("should change name", () => {
+    const item = { name: "sword", enhancement: 15, durability: 80 };
+    const result = enhancer.get(item);
+    expect(result.name).toBe("[+15] sword");
+  });
+    it("should not change name if 0", () => {
+      const item = { name: "sword", enhancement: 0, durability: 80 };
+      const result = enhancer.get(item);
+      expect(result.name).toBe("sword");
+    });
+});
