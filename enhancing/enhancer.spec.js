6df72d7a1369d58cc8a950fffc1437fe8f181212
Sunil Karki <karkisunil1200@gmail.com>
2020-05-21 09:48:24 -0500

    repaire test done

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,13 @@
 const enhancer = require('./enhancer.js');
 // test away
+
+describe('enhancer', () => {
+  describe('repair', () => {
+    it('should return a new item with the durablity restored to 100', () => {
+      const item = {name: 'itemA', durability: 29, enhancement: 20};
+      const newItem = enhancer.repair(item);
+
+      expect(newItem.durability).toBe(100);
+    });
+  });
+});
