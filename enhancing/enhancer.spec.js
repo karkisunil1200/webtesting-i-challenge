b3856a2c98a56fd4ab9aab23c787a7d0f95e83ad
Sunil Karki <karkisunil1200@gmail.com>
2020-05-21 10:31:45 -0500

    Success done

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,13 +1,20 @@
 const enhancer = require('./enhancer.js');
 // test away
 
-describe('enhancer', () => {
-  describe('repair', () => {
-    it('should return a new item with the durablity restored to 100', () => {
-      const item = {name: 'itemA', durability: 29, enhancement: 20};
-      const newItem = enhancer.repair(item);
+describe('enhancer repaire', () => {
+  it('should return a new item with the durablity restored to 100', () => {
+    const item = {name: 'itemA', durability: 29, enhancement: 20};
+    const newItem = enhancer.repair(item);
 
-      expect(newItem.durability).toBe(100);
-    });
+    expect(newItem.durability).toBe(100);
+  });
+});
+
+describe('enhancer success', () => {
+  it('The item enhancement increases by 1', () => {
+    const item = {name: 'itemA', durability: 19, enhancement: 10};
+    const newItem = enhancer.succeed(item);
+
+    expect(newItem.enhancement).toBe(11);
   });
 });
